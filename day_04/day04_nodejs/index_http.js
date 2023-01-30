612d29165fe44339e5d8f6db402e9a824bbf3b1e
Lee <tryagain100@cu.ac.kr>
2023-01-30 16:49:46 +0900

    Day04

---
+++ day_04/day04_nodejs/index_http.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+const server = http.createServer(function(req, res) {
+    res.end("<h1>Hi</h2>");
+
+});
+
+server.listen(3000,function(){
+    console.log("서버 실행중");
+});
