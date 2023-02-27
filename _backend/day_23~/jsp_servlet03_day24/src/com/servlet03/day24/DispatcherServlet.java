073b5ac29be969e5fd1e8f950d27b51a4f09489f
Lee <tryagain100@cu.ac.kr>
2023-02-27 16:51:36 +0900

    Day24 servlet

--- _backend/day_23~/jsp_servlet03_day24/src/com/servlet03/day24/DispatcherServlet.java
+++ _backend/day_23~/jsp_servlet03_day24/src/com/servlet03/day24/DispatcherServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.servlet03.day24.model.PersonDTO;
+
 
 public class DispatcherServlet extends HttpServlet {
 	@Override
