eebabf69f14183bf2a486f16157c8f468721d96d
Lee <tryagain100@cu.ac.kr>
2023-03-24 16:35:25 +0900

    sample codes

---
+++ _backend/SpringBoot/JPA_AssociationMapping4/src/main/java/com/springboot/map/JpaAssociationMapping4Application.java
@@ -0,0 +1,13 @@
+package com.springboot.map;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAssociationMapping4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAssociationMapping4Application.class, args);
+	}
+
+}
